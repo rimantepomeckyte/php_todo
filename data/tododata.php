c1302a712168f40f3cbf70b03c84baa1256b49aa
rimantepomeckyte <rimante.pomeckyte@gmail.com>
2021-01-26 11:10:49 +0200

    first

---
+++ data/tododata.php
@@ -0,0 +1,19 @@
+<?php
+$priority = [
+"low",
+    "normal",
+    "high"
+];
+
+$percentCompleted = [
+    0,
+    25,
+    50,
+    75,
+    100
+];
+$status = [
+    "new",
+    "in progress",
+    "complete"
+];
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require "vendor/autoload.php";
+require "data/tododata.php";
+require_once 'view/index.view.php';
